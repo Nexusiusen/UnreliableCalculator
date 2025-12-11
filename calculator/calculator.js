50c410ddb759a9c30e9b7624ee8b077e7a370f1d
Nexusiusen <Nexus.wikblad@gmail.com>
2025-12-11 01:51:36 +0100

    Revert "stuff"
    This reverts commit 470bdbe5889f548fedd01d04b7e2d05d9c2015b4.

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,14 +1,13 @@
-let number ="";
-let operator ="";
-let a ="";
-let answer=false;
+let number = "";
+let operator = "";
+let a = "";
+let answer = false;
 
-const wrapper = document.getElementById('wrapper');
+const wrapper = document.getElementById("wrapper");
 
-wrapper.addEventListener('click', (event) => {
-  const isButton = event.target.nodeName === 'BUTTON';
-  if (!isButton) 
-  {
+wrapper.addEventListener("click", (event) => {
+  const isButton = event.target.nodeName === "BUTTON";
+  if (!isButton) {
     return;
   }
 
@@ -17,87 +16,70 @@ wrapper.addEventListener('click', (event) => {
 
   // Log the button ID to the console
   console.log(`Button clicked: ${buttonId}`);
- 
-if (buttonId == "+" || buttonId == "-"||buttonId == "*"||buttonId=="/")
-{
-	operator=buttonId;
 
-  if (a =="")
-  {
-    a=number;
-    number ="";
+  if (
+    buttonId == "+" ||
+    buttonId == "-" ||
+    buttonId == "*" ||
+    buttonId == "/"
+  ) {
+    operator = buttonId;
+
+    if (a == "") {
+      a = number;
+      number = "";
+    } else if (number != "") {
+      a = calculate(a, number, operator);
+      number = "";
+    }
+  } else if (buttonId == "=") {
+    if (operator != "") {
+      a = calculate(a, number, operator);
+      number = "";
+      operator = "";
+      answer = true;
+    }
+  } else if (buttonId == "C") {
+    number = "";
+    operator = "";
+    a = "";
+  } else {
+    if (answer == true && operator == "") {
+      a = "";
+      answer = false;
+    }
+    number += buttonId;
   }
 
-  else if(number!="")
-  {
-    a=calculate(a,number,operator)
-    number ="";
-  }
-}
-
-else if (buttonId == "=")
-{
-  if(operator !="")
-  {
-    a=calculate(a,number,operator)
-    number="";
-    operator="";
-    answer=true;
-
-    
-  }
-} 
-
-else if (buttonId == "C")
-{
-  number="";
-  operator="";
-  a="";
-}
-
-else
-{
-  if (answer==true && operator=="")
-  {
-    a="";
-    answer=false;
-  }
-  number +=buttonId;
-}
-  
-
-
-function calculate(num1,num2,operator)
-{
-  //da funny
-  const negOrPos = () => Math.floor(Math.random() * 2) === 1 ? -1 : 1;
-  const modifier = (Math.floor(Math.random() * 5)) * negOrPos();
-  const truthGenerator = () => Math.floor(Math.random() * 5) === 3 ? "false" : "truth";
-  const truth = document.getElementById('truth');
-    if (truth) 
-    {
+  function calculate(num1, num2, operator) {
+    //da funny
+    const negOrPos = () => (Math.floor(Math.random() * 2) === 1 ? -1 : 1);
+    const modifier = Math.floor(Math.random() * 5) * negOrPos();
+    const truthGenerator = () =>
+      Math.floor(Math.random() * 5) === 3 ? "false" : "truth";
+    const truth = document.getElementById("truth");
+    if (truth) {
       truth.textContent = truthGenerator();
     }
     //end of funny
 
-  switch (operator)
-  {
-  case "+":
-    return (parseInt(a)+parseInt(number))+modifier;
-    break;
-  case "-":
-    return(parseInt(a)-parseInt(number))+modifier;
-    break;
-  case "*":
-    return (parseInt(a)*parseInt(number))+modifier;
-    break;
-  case "/":
-    return (parseInt(a)/parseInt(number))+modifier;
-    break;
+    switch (operator) {
+      case "+":
+        return parseInt(a) + parseInt(number) + modifier;
+        break;
+      case "-":
+        return parseInt(a) - parseInt(number) + modifier;
+        break;
+      case "*":
+        return parseInt(a) * parseInt(number) + modifier;
+        break;
+      case "/":
+        return parseInt(a) / parseInt(number) + modifier;
+        break;
+    }
   }
-}
 
-const display = document.getElementById('display');
+  const display = document.getElementById("display");
   if (display) {
     display.textContent = a + operator + number;
   }
